fba52d1eaaa99e6fd7d70eb8395a809f760ba361
emmageo3 <kanfanyemma22@gmail.com>
2022-02-28 12:10:04 +0000

    react app done

--- studentcrud/src/pages/Navbar.js
+++ studentcrud/src/pages/Navbar.js
@@ -7,7 +7,7 @@ function Navbar ()
     <div className="pb-5">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <Link className="navbar-brand" to="/">Navbar</Link>
+          <Link className="navbar-brand" to="/">Schoolapp</Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
