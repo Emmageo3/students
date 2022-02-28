7fc01c36c8c65cf2206f5a0e2b98e4fd25f4e81a
emmageo3 <kanfanyemma22@gmail.com>
2022-02-28 12:19:57 +0000

    configuration de API

--- laravelapp/routes/api.php
+++ laravelapp/routes/api.php
@@ -3,16 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\API\StudentController;
+
+Route::get('students', [StudentController::class, 'index']);
+Route::post('/add-student', [StudentController::class, 'store']);
+Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
+Route::put('update-student/{id}', [StudentController::class, 'update']);
+Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
